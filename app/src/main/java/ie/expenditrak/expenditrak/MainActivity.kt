384a0b2079894766c0125e8eb20364188bb9d979
Ellie <elliemorrissey22@gmail.com>
2019-03-01 13:52:29 +0000

    initial commit

---
+++ app/src/main/java/ie/expenditrak/expenditrak/MainActivity.kt
@@ -0,0 +1,11 @@
+package ie.expenditrak.expenditrak
+
+import...
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
